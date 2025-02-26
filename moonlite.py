9536480531bdb8a5c748ca072569b2ffc7c08044
brigibankai <peenoybee@gmail.com>
2025-02-26 02:38:28 -0800

    initial 230 am git commit. lol

---
+++ moonlite.py
@@ -0,0 +1,37 @@
+# impport necassary libarries
+
+# define a fuction to handle CLI input
+
+    # set up arguemnt parser
+
+    # add argumnet for query input
+
+    # parse the arugment s
+
+# define a fuction to load vecotr database
+
+    # check if databsae exists
+
+    # if not, creat it
+
+    # return databse connction
+
+# define a fuction to process querries
+
+    # take user inpt
+
+    #convrt input into vectoor embedding
+
+    # serch for closest matcing results in db
+
+    # retrun results
+
+# define main fuction
+
+    # check if user proivded a querry
+
+    # if so, process it
+
+    # else , show hlp msg
+
+# excute main fuction if script is run direcly
